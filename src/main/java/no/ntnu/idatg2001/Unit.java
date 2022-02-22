2a64a3585226540dff40500edd04fb455cebc7bb
sondesp <sondesp@stud.ntnu.no>
2022-02-22 18:43:46 +0100

    Implemented constructor and methods in Unit

--- src/main/java/no/ntnu/idatg2001/Unit.java
+++ src/main/java/no/ntnu/idatg2001/Unit.java
@@ -1,9 +1,82 @@
 package no.ntnu.idatg2001;
 
+/**
+ * Superclass for units, given their name and statistics.
+ *
+ * @author sondesp
+ * @version 0.0.1
+ * @since 0.0.1
+ */
+
 public abstract class Unit {
 
     private String name;
     private int health;
     private int attack;
     private int armor;
+
+    /**
+     * Constructor for unit
+     * @param name Short descriptive name. Ex: "Archer", "Swordsman"
+     * @param health Health of unit, greater than 0
+     * @param attack Attack value represents the weapons damage
+     * @param armor Armor value represents the units defence
+     */
+
+    public Unit(String name, int health, int attack, int armor) {
+        this.name = name;
+        this.health = health;
+        this.attack = attack;
+        this.armor = armor;
+    }
+
+    /**
+     * @return Name of unit
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @return Health of unit
+     */
+    public int getHealth() {
+        return health;
+    }
+
+    /**
+     * @return Attack of unit
+     */
+    public int getAttack() {
+        return attack;
+    }
+
+    /**
+     * @return Armor of unit
+     */
+    public int getArmor() {
+        return armor;
+    }
+
+    /**
+     * @param health Sets health of unit
+     */
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    /**
+     * @return String of fields
+     */
+    public String toString() {
+        return "Unit{" +
+                "name='" + name + '\'' +
+                ", health=" + health +
+                ", attack=" + attack +
+                ", armor=" + armor +
+                '}';
+    }
+
+    public abstract int getAttackBonus();
+    public abstract int getResistBonus();
 }
