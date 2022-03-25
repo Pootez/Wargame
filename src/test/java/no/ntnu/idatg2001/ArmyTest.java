0abdeb4bfe5d14183a5c336e07ab7acb10454e88
Sondre Espeland <sondesp@stud.ntnu.no>
2022-03-26 00:55:40 +0100

    Added tests for filtering methods.

---
+++ src/test/java/no/ntnu/idatg2001/ArmyTest.java
@@ -0,0 +1,56 @@
+package no.ntnu.idatg2001;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ArmyTest {
+
+    private Army army;
+
+    @BeforeEach
+    void setup() {
+        army = new Army("Larpers Unite");
+
+        army.addUnit(new CommanderUnit("Bob Lee the Feared",240));
+
+        army.addUnit(new RangedUnit("Archer",40));
+        army.addUnit(new RangedUnit("Archer",40));
+        army.addUnit(new RangedUnit("Archer",40));
+        army.addUnit(new RangedUnit("Archer",40));
+        army.addUnit(new RangedUnit("Archer",40));
+
+        army.addUnit(new CavalryUnit("Swordsman",40));
+        army.addUnit(new CavalryUnit("Swordsman",40));
+        army.addUnit(new CavalryUnit("Swordsman",40));
+        army.addUnit(new CavalryUnit("Swordsman",40));
+        army.addUnit(new CavalryUnit("Swordsman",40));
+        army.addUnit(new CavalryUnit("Swordsman",40));
+        army.addUnit(new CavalryUnit("Swordsman",40));
+        army.addUnit(new CavalryUnit("Swordsman",40));
+
+        army.addUnit(new InfantryUnit("Jeremy from accounting",20));
+        army.addUnit(new InfantryUnit("Greg, the General Store Manager",20));
+    }
+
+    @Test
+    void getInfantryUnits() {
+        assertEquals(2,army.getInfantryUnits().size());
+    }
+
+    @Test
+    void getCavalryUnits() {
+        assertEquals(8,army.getCavalryUnits().size());
+    }
+
+    @Test
+    void getRangedUnits() {
+        assertEquals(5,army.getRangedUnits().size());
+    }
+
+    @Test
+    void getCommanderUnits() {
+        assertEquals(1,army.getCommanderUnits().size());
+    }
+}
