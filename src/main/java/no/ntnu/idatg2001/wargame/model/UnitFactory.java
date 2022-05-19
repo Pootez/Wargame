b9539157c661a37f8607b8d6ebf45434938dceb0
Sondre Espeland <sondesp@stud.ntnu.no>
2022-05-19 21:17:58 +0200

    Documentation for UnitFactory

--- src/main/java/no/ntnu/idatg2001/wargame/model/UnitFactory.java
+++ src/main/java/no/ntnu/idatg2001/wargame/model/UnitFactory.java
@@ -3,13 +3,29 @@ package no.ntnu.idatg2001.wargame.model;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Class for creating a single or a number of units of the same type, name and hp
+ *
+ * @author Sondre Espeland
+ * @version 2022-05-19
+ * @since 2022-05-19
+ */
+
 public class UnitFactory {
+
+    /**
+     * Method for creating n number of units of the same type, name and hp.
+     *
+     * @param n number of units
+     * @param type type of unit
+     * @param name name of unit
+     * @param health health point of unit
+     * @return List of units
+     */
 
     public List<Unit> createUnits(int n, String type, String name, int health) {
         if (name.contains(",")) {throw new IllegalArgumentException("Unit name cannot contain ','");}
-        if (n <= 0) {
-            return new ArrayList<>();
-        }
+        if (n <= 0) {throw new IllegalArgumentException("number of units must be greater than 0");}
 
         ArrayList<Unit> units = new ArrayList<>();
         for (int i = 0; i < n; i++) {
@@ -17,6 +33,15 @@ public class UnitFactory {
         }
         return units;
     }
+
+    /**
+     * Method for creating a unit.
+     *
+     * @param type type of unit
+     * @param name name of unit
+     * @param health health point of unit
+     * @return Single Unit
+     */
 
     public Unit createUnit(String type, String name, int health) {
         if (name.contains(",")) {throw new IllegalArgumentException("Unit name cannot contain ','");}
