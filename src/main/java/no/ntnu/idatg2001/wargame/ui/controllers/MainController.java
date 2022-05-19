e3969ad98bc7e1e171bbd38b9fe7a8d6e42cb4b3
Sondre Espeland <sondesp@stud.ntnu.no>
2022-05-19 18:21:04 +0200

    Added classes for the main window and its controller

---
+++ src/main/java/no/ntnu/idatg2001/wargame/ui/controllers/MainController.java
@@ -0,0 +1,5 @@
+package no.ntnu.idatg2001.wargame.ui.controllers;
+
+public class MainController {
+
+}
---
+++ src/main/java/no/ntnu/idatg2001/wargame/ui/views/MainWindow.java
@@ -0,0 +1,19 @@
+package no.ntnu.idatg2001.wargame.ui.views;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class MainWindow extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+
+    }
+
+}
