6e9b0f0d716b5282b42f63647e5e9f1a3cfff777
Sondre Espeland <sondesp@stud.ntnu.no>
2022-05-19 18:41:56 +0200

    Implementing MainWindow into Main class for packaging

--- src/main/java/no/ntnu/idatg2001/wargame/Main.java
+++ src/main/java/no/ntnu/idatg2001/wargame/Main.java
@@ -1,7 +1,10 @@
 package no.ntnu.idatg2001.wargame;
 
+import no.ntnu.idatg2001.wargame.ui.views.MainWindow;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Main initialized...");
+        MainWindow.main(args);
     }
 }
