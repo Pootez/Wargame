acacca3b58f66e65bb51d3cda4bebb402064e66e
Sondre Espeland <sondesp@stud.ntnu.no>
2022-05-19 18:29:54 +0200

    Implemented basic StackPane scene

--- src/main/java/no/ntnu/idatg2001/wargame/ui/views/MainWindow.java
+++ src/main/java/no/ntnu/idatg2001/wargame/ui/views/MainWindow.java
@@ -1,6 +1,8 @@
 package no.ntnu.idatg2001.wargame.ui.views;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -13,7 +15,10 @@ public class MainWindow extends Application {
 
     @Override
     public void start(Stage primaryStage) throws IOException {
+        StackPane stackPane = new StackPane();
 
+        Scene scene = new Scene(stackPane);
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
-
 }
