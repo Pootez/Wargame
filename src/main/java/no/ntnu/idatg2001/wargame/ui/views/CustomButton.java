7d8bd875375a744133bfe9bac40ca33785c68260
Sondre Espeland <sondesp@stud.ntnu.no>
2022-05-23 07:24:51 +0200

    CustomButton documentation

--- src/main/java/no/ntnu/idatg2001/wargame/ui/views/CustomButton.java
+++ src/main/java/no/ntnu/idatg2001/wargame/ui/views/CustomButton.java
@@ -5,10 +5,26 @@ import javafx.geometry.Insets;
 import javafx.scene.control.Button;
 import javafx.scene.text.Font;
 
-public class CustomButton extends Button{
-    public CustomButton (String name, EventHandler e) {
+/**
+ * Custom button with inbuilt name and setOnAction.
+ * Extends Button
+ *
+ * @author sondesp
+ * @version 2022-05-23
+ * @since 2022-05-23
+ */
+
+public class CustomButton extends Button {
+
+    /**
+     * Constructor for the custom button.
+     *
+     * @param name String, text displayed on button.
+     * @param event EventHandler, for using methods (e -> {})
+     */
+    public CustomButton (String name, EventHandler event) {
         super(name);
-        this.setOnAction(e);
+        this.setOnAction(event);
         this.setFont(Font.font(20));
         this.setPadding(new Insets(5,30,5,30));
     }
